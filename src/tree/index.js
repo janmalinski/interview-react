a0518d32187a16b881a878718796c3ee7297636c
Jan Malinski <janmalinski@macbook-pro-jan.home>
2023-08-19 14:30:39 +0200

    redraw tree upside down

--- src/tree/index.js
+++ src/tree/index.js
@@ -7,7 +7,7 @@ import data from './data.json'
 const Tree = () => {
     return (
             <div className="tree">
-                {data.map((item) => (
+                {data.reverse().map((item) => (
                     <div key={item.id}>
                         <><span className={item.parentId === 1 ? 'four-spaces-left' : item.parentId === 3 ? 'eight-spaces-left' : item.parentId === 5 ? 'twelve-spaces-left' :  undefined}>{item.name}</span><br/></>
                     </div>
