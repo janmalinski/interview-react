40117a36da48daaebce0d3d54805b0d635a7ad81
Jan Malinski <janmalinski@macbook-pro-jan.home>
2023-08-20 12:28:50 +0200

    add text inputs

--- src/tree/index.js
+++ src/tree/index.js
@@ -4,22 +4,29 @@ import axios from 'axios'
 import './index.css'
 import data from './data.json'
 
+
+
 const Tree = () => {
+
     return (
             <div className="tree">
                 {data.map((item) => {
-                    const zeroLevel = item.parentId === undefined;
-                    const firstLevel = item.parentId === 1;
-                    const secondLevel = item.parentId === 3;
-                    const thirdLevel = item.parentId === 5;
-                    const name = item.name;
-                    const firstLetter= name[0];
-                    const wordWithoutFirstLetter = name.substring(1);
+                    const zeroLevel = item.parentId === 1;
+                    const firstLevel = item.parentId === 3;
+                    const secondLevel = item.parentId === 5;
+
+                    const lastZeroLevel = item.name === 'frog';
+                    const lastFirstLevel = item.name === 'dog';
+                    const lastSecondLevel = item.name === 'elephant';
+                
                     return (
                     <div key={item.id}>
-                        <><span className={firstLevel ? 'four-spaces-left' : secondLevel ? 'eight-spaces-left' : thirdLevel ? 'twelve-spaces-left' :  undefined}>
-                        { zeroLevel ? name :  firstLevel ? firstLetter + '.' + wordWithoutFirstLetter : secondLevel ? firstLetter + '..' + wordWithoutFirstLetter : thirdLevel && firstLetter + '...' + wordWithoutFirstLetter}
+                        <><span className={zeroLevel ? 'four-spaces-left' : firstLevel ? 'eight-spaces-left' : secondLevel ? 'twelve-spaces-left' :  undefined}>
+                        {item.name}
                         </span><br/></>
+                         {lastZeroLevel && <input className='four-spaces-left' type="text" id="word" name="word"/> }
+                         {lastFirstLevel && <input className='eight-spaces-left' type="text" id="word" name="word"/> }
+                         {lastSecondLevel && <input className='twelve-spaces-left' type="text" id="word" name="word"/> }
                     </div>
                     )
                 }
