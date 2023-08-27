4500ded9654b71ca4ff2fadd1a85e8a78ed3520b
Jan Malinski <janmalinski@macbook-pro-jan.home>
2023-08-27 11:58:22 +0200

    add and remove item functions, improve display of text inputs

--- src/tree/index.js
+++ src/tree/index.js
@@ -2,36 +2,84 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
 import './index.css'
-import data from './data.json'
-
-
+import dataJson from './data.json'
 
 const Tree = () => {
+
+    const [data, setData] = useState([]);
+    const [currentId, setCurrentId] = useState(null);
+
+    useEffect(() => {
+        setData(dataJson);
+    }, [dataJson]);
+
+    const addItem = (event) => {
+            if (event.key === 'Enter' && event.target.value !== '') {
+
+                const parentItem = data.find(item => item.id === currentId);
+
+                const item = {
+                    id: currentId + 1,
+                    name: event.target.value,
+                    parentId: parentItem.parentId,
+                };
+    
+                const currentItemIndex = data.findIndex(item => item.id === currentId);
+                
+                data.splice(currentItemIndex + 1, 0, item);
+    
+                const updatedArray = [];
+    
+                updatedArray.push(data);
+            
+                const doubledIdObjects = updatedArray[0].filter(item => (item.id >= currentId + 1 && item.name !== event.target.value));
+    
+                const restWithoutDoubledIdObjects = updatedArray[0].filter(item => (item.id < currentId + 1));
+    
+                const updatedDoubledIdObjects = doubledIdObjects.map(item => ({id: item.id + 1, name: item.name, parentId: item.parentId}))
+    
+                const updatedData  = [];
+                updatedData.push(...restWithoutDoubledIdObjects);
+                updatedData.push(item);
+                updatedData.push(...updatedDoubledIdObjects);
+                setData(updatedData);
+                event.target.value = null;
+            };      
+    };
+
+    const removeItem = (id) => {
+        const dataWithoutRemovedItem = data.filter(item => item.id !== id);
+        setData(dataWithoutRemovedItem);
+    };
 
     return (
             <div className="tree">
                 {data.map((item) => {
-                    const zeroLevel = item.parentId === 1;
-                    const firstLevel = item.parentId === 3;
-                    const secondLevel = item.parentId === 5;
 
-                    const lastZeroLevel = item.name === 'frog';
-                    const lastFirstLevel = item.name === 'dog';
-                    const lastSecondLevel = item.name === 'elephant';
-                
-                    return (
+                    const zeroLevel = item.parentId === 1 
+                    const firstLevel = item.parentId === 3 
+                    const secondLevel = item.parentId === 5 
+
+                    const itemsZeroLevel = data.filter(item => item.parentId === 1);
+                    const lastItemZeroLevelId =  itemsZeroLevel[itemsZeroLevel.length - 1] && itemsZeroLevel[itemsZeroLevel.length - 1].id;
+
+                    const itemsFirstLevel = data.filter(item => item.parentId === 3);
+                    const lastItemFirstLevelId =  itemsFirstLevel[itemsFirstLevel.length - 1] && itemsFirstLevel[itemsFirstLevel.length - 1].id;
+
+                    const itemsSecondLevel = data.filter(item => item.parentId === 5);
+                    const lastItemSecondLevelId =  itemsSecondLevel[itemsSecondLevel.length - 1] && itemsSecondLevel[itemsSecondLevel.length - 1].id;
+                    
+                return (
                     <div key={item.id}>
                         <><span className={zeroLevel ? 'four-spaces-left' : firstLevel ? 'eight-spaces-left' : secondLevel ? 'twelve-spaces-left' :  undefined}>
-                        {item.name}
+                        {item.name} <button type='button' onClick={()=>removeItem(item.id)} style={{border: 0}}> ❌</button>
                         </span><br/></>
-                         {lastZeroLevel && <input className='four-spaces-left' type="text" id="word" name="word"/> }
-                         {lastFirstLevel && <input className='eight-spaces-left' type="text" id="word" name="word"/> }
-                         {lastSecondLevel && <input className='twelve-spaces-left' type="text" id="word" name="word"/> }
+                         {lastItemZeroLevelId === item.id && <input className='four-spaces-left' type="text" onKeyDown={addItem} onChange={()=>setCurrentId(item.id)}/> }
+                         {lastItemFirstLevelId === item.id && <input className='eight-spaces-left' type="text" onKeyDown={addItem} onChange={()=>setCurrentId(item.id)}/> }
+                         {lastItemSecondLevelId === item.id && <input className='twelve-spaces-left' type="text" onKeyDown={addItem} onChange={()=>setCurrentId(item.id)}/> }
                     </div>
-                    )
-                }
-                    
-                )}
+                    );
+            })}
             </div>
     );
 };
